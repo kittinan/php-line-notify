f7448189984c501251a737e79e095364ca747d59
Kittinan Srithaworn <tunxedge@gmail.com>
2017-01-15 12:55:42 +0700

    - Line notify (text, image)

---
+++ README.md
@@ -0,0 +1,39 @@
+php-line-notify
+========
+[![Build Status](https://travis-ci.org/kittinan/php-line-notify.svg?branch=master)](https://travis-ci.org/kittinan/php-line-notify)
+[![Code Coverage](https://scrutinizer-ci.com/g/kittinan/php-line-notify/badges/coverage.png?b=master)](https://scrutinizer-ci.com/g/kittinan/php-line-notify/?branch=master)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/kittinan/php-line-notify/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/kittinan/php-line-notify/?branch=master)
+[![License](https://poser.pugx.org/kittinan/php-line-notify/license.svg)](https://packagist.org/packages/kittinan/php-line-notify)
+
+Line Notify with PHP 
+
+[Line Notify Document] (https://notify-bot.line.me/doc/en/)
+
+
+
+## Requirement
+* PHP 5.5+
+* [guzzlehttp] (https://github.com/guzzle/guzzle)
+
+## Composer
+
+Install the latest version with composer require kittinan/php-line-notify
+
+This plugin on the Packagist.
+
+[https://packagist.org/packages/kittinan/php-line-notify](https://packagist.org/packages/kittinan/php-line-notify)
+
+## Usage
+*Example : notify text message*
+```php
+$token = 'YOUR LINE NOTIFY TOKEN';
+$ln = new KS\Line\LineNotify($token);
+
+$text = 'Hello Line Notify';
+$ln->sendText($text);
+```
+
+
+License
+=======
+The MIT License (MIT)
---
+++ src/LineNotify.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace KS\Line;
+
+use GuzzleHttp\Client;
+
+class LineNotify {
+
+  const API_URL = 'https://notify-api.line.me/api/notify';
+
+  private $token = null;
+  private $http = null;
+
+  public function __construct($token) {
+
+    $this->token = $token;
+    $this->http = new Client();
+  }
+
+  public function setToken($token) {
+    $this->token = $token;
+  }
+
+  public function getToken() {
+    return $this->token;
+  }
+
+  public function sendText($text, $imagePath = null) {
+
+    if (empty($text)) {
+      return false;
+    }
+
+    $request_params = [
+      'headers' => [
+        'Authorization' => 'Bearer ' . $this->token,
+      ],
+    ];
+
+    if (!empty($imagePath)) {
+      $request_params['multipart'] = [
+        [
+          'name' => 'message',
+          'contents' => $text
+        ],
+        [
+          'name' => 'imageFile',
+          'contents' => fopen($imagePath, 'r')
+        ],
+      ];
+    } else {
+      $request_params['form_params'] = ['message' => $text];
+    }
+
+    $response = $this->http->request('POST', LineNotify::API_URL, $request_params);
+
+    if ($response->getStatusCode() != 200) {
+      return false;
+    }
+
+    $body = (string) $response->getBody();
+    $json = json_decode($body, true);
+    if (empty($json['status']) || empty($json['message'])) {
+      return false;
+    }
+
+    return true;
+  }
+
+}
