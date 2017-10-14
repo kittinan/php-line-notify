ffc1852f6ee83576c8a86970400d142c5ac03e83
Kittinan Srithaworn <tunxedge@gmail.com>
2017-10-14 20:28:57 +0700

    Issue #2 method description

--- src/LineNotify.php
+++ src/LineNotify.php
@@ -4,27 +4,59 @@ namespace KS\Line;
 
 use GuzzleHttp\Client;
 
+/**
+ * A simple class to send text message, image and Line sticker on Line notify
+ * 
+ * https://notify-bot.line.me/doc/en/
+ * 
+ * @property string $token Line notify token
+ * @property \GuzzleHttp\Client $http Guzzle Http Client instance for send Http request
+ */
 class LineNotify {
 
   const API_URL = 'https://notify-api.line.me/api/notify';
 
   private $token = null;
   private $http = null;
-
+  
+  /**
+   * Initialize class with Line notify token string
+   * 
+   * @param string $token the token of Line notify
+   */
   public function __construct($token) {
 
     $this->token = $token;
     $this->http = new Client();
   }
-
+  
+  /**
+   * Set token Line notify that want to send message
+   * 
+   * @param string $token the token of Line notify
+   */
   public function setToken($token) {
     $this->token = $token;
   }
-
+  
+  /**
+   * Get current token Line Notify
+   * 
+   * @return string the token of Line notify
+   */
   public function getToken() {
     return $this->token;
   }
-
+  
+  /**
+   * Send text message, image or sticker on Line notify
+   * 
+   * @param string $text text message on Line notify can not be empty
+   * @param string $imagePath image path you want to send on the local machine
+   * @param array() $sticker array of line sticker ['stickerPackageId' => PACKAGE_ID, 'stickerId' => STICKER_ID ] 
+   *                         more info https://devdocs.line.me/files/sticker_list.pdf
+   * @return boolean success or fail on send Line notify message
+   */
   public function send($text, $imagePath = null, $sticker = null) {
 
     if (empty($text)) {
